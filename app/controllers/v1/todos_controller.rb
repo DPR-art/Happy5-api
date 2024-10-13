a050e5375e04532c87c29694ded0b0fa264f74d9
Dani <danideni173@gmail.com>
2024-10-13 17:34:16 +0700

    add fitur

--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -1,53 +1,48 @@
 module V1
   class TodosController < ApplicationController
-    # [...]
+    before_action :set_todo, only: [:show, :update, :destroy]
+    
     # GET /todos
     def index
       # get current user todos
       @todos = current_user.todos
       json_response(@todos)
     end
-    # def index
-    #   # get paginated current user todos
-    #   @todos = current_user.todos.paginate(page: params[:page], per_page: 2)
-    #   json_response(@todos)
-    # end
-    
-    # [...]
+
     # POST /todos
     def create
       # create todos belonging to current user
       @todo = current_user.todos.create!(todo_params)
       json_response(@todo, :created)
     end
-    
+
     # GET /todos/:id
     def show
-      @todo = current_user.todos.create!(todo_params)
       json_response(@todo)
     end
 
     # PUT /todos/:id
     def update
-      @todo = current_user.todos.create!(todo_params)
-      @todo.update(todo_params)
+      @todo.update!(todo_params)
       head :no_content
     end
 
     # DELETE /todos/:id
     def destroy
-      @todo = current_user.todos.create!(todo_params)
       @todo.destroy
       head :no_content
     end
 
-    # [...]
     private
 
-    # remove `created_by` from list of permitted parameters
+    # Find todo by id for actions that require it
+    def set_todo
+      @todo = current_user.todos.find(params[:id])
+    end
+
+    # Strong parameters to allow only title to be updated/created
     def todo_params
       params.permit(:title)
     end
-    # [...]
   end
 end
